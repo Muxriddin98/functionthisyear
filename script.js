7b235c02b6a20a6b7f02f7e8561dce38323e018d
muxriddin <mukhir98@gmail.com>
2022-07-21 18:27:27 +0500

    function

---
+++ script.js
@@ -0,0 +1,16 @@
+let name = prompt('Напишите своё имя')
+let age = +prompt ('Напишите шод рождения')
+let thisyear = +prompt ('Напишите нынешний год')
+
+alert(name + ',  '  + ' Ваш возраст ' + (thisyear - age))
+
+
+
+
+
+
+
+
+
+
+
